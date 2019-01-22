3d9de0c4dcaa79b65cb37dc7d4064aa45fe1997a
Denis Moreno <denis.moreno@inivata.com>
2019-01-22 14:28:30 +0000

    add util to dump xml

--- genologics/test_utils.py
+++ genologics/test_utils.py
@@ -42,3 +42,16 @@ def patched_get(*args, **kwargs):
         return ElementTree.fromstring(XML_DICT[r.url])
     except KeyError:
         raise Exception("Cannot find mocked xml for uri {0}".format(r.url))
+
+def dump_source_xml(lims):
+    """After using a LIMS object, using this method on it will dump all the cached XML in a serialized dictionnary form,
+    to be used with patched_get"""
+    final_string = []
+    final_string.append('{')
+    for k, v in lims.cache.iteritems():
+        final_string.append("'{0}':".format(k))
+        v.get()
+        final_string.append('"""{0}""",'.format(v.xml().replace('\n', "\n")))
+    final_string.append('}')
+
+    return '\n'.join(final_string)
