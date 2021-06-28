f2438809cc4ee5baac28060ef6d7251c96d32c55
Steinar Sturlaugsson <costeinar@gmail.com>
2021-06-28 16:23:05 +0200

    Fix warnings when running pytest

--- genologics/config.py
+++ genologics/config.py
@@ -1,10 +1,7 @@
 import os
+import sys
+import warnings
 from configparser import ConfigParser
-
-try:
-    from configparser import SafeConfigParser
-except ImportError:
-    from configparser import SafeConfigParser
 
 
 
@@ -15,7 +12,7 @@ from genologics.config import BASEURI, USERNAME, PASSWORD
 Alternate Usage:
 from genologics import config
 BASEURI, USERNAME, PASSWORD, VERSION, MAIN_LOG = config.load_config(specified_config = <path to config file>)
-"""
+'''
 
 spec_config = None
 
@@ -24,20 +21,22 @@ def get_config_info(config_file):
     config = ConfigParser()
     config.read_file(open(config_file))
 
-    BASEURI = config.get("genologics", "BASEURI").rstrip()
-    USERNAME = config.get("genologics", "USERNAME").rstrip()
-    PASSWORD = config.get("genologics", "PASSWORD").rstrip()
 
-    if config.has_section("genologics") and config.has_option("genologics", "VERSION"):
-        VERSION = config.get("genologics", "VERSION").rstrip()
+    BASEURI = config.get('genologics', 'BASEURI').rstrip()
+    USERNAME = config.get('genologics', 'USERNAME').rstrip()
+    PASSWORD = config.get('genologics', 'PASSWORD').rstrip()
+
+    if config.has_section('genologics') and config.has_option('genologics','VERSION'):
+        VERSION = config.get('genologics', 'VERSION').rstrip()
     else:
-        VERSION = "v2"
+        VERSION = 'v2'
 
-    if config.has_section("logging") and config.has_option("logging", "MAIN_LOG"):
-        MAIN_LOG = config.get("logging", "MAIN_LOG").rstrip()
+    if config.has_section('logging') and config.has_option('logging','MAIN_LOG'):
+        MAIN_LOG = config.get('logging', 'MAIN_LOG').rstrip()
     else:
         MAIN_LOG = None
     return BASEURI, USERNAME, PASSWORD, VERSION, MAIN_LOG
+
 
 
 def load_config(specified_config=None):
@@ -65,6 +64,7 @@ def load_config(specified_config=None):
     BASEURI, USERNAME, PASSWORD, VERSION, MAIN_LOG = get_config_info(config_file)
 
     return BASEURI, USERNAME, PASSWORD, VERSION, MAIN_LOG
+
 
 
 BASEURI, USERNAME, PASSWORD, VERSION, MAIN_LOG = load_config(
